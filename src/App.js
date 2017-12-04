d988ca6f15f9e00c9cef4ad17323f192418600cf
Swizec Teller <swizec@swizec.com>
2017-12-04 14:52:31 -0500

    Ticket with proper data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { Ticket } from "./Ticket";
+import data from "./data.json";
+
+console.log(data);
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Ticket />
+                <Ticket info={data.events[0]} />
             </div>
         );
     }
--- src/Ticket.js
+++ src/Ticket.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { format } from "date-fns";
 
 class Ticket extends Component {
     render() {
+        const {
+            info: { imageUrl, name, description, eventDateLocal }
+        } = this.props;
+
         return (
             <div>
-                <img src={logo} style={{ width: "150px" }} />
+                <img src={imageUrl} style={{ width: "350px" }} />
                 <div>
-                    <h2>Ticket title</h2>
-                    <p>Ticket description</p>
+                    <h2>{name}</h2>
+                    <p>{format(eventDateLocal, "ddd Do MMMM, hh:mma")}</p>
+                    <p>{description}</p>
                 </div>
             </div>
         );
